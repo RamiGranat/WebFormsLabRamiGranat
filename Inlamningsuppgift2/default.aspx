9efdbb3f3468ad71ab987ffcf0808ff4e2d3c865
RamiGranat <raextheassassin@gmail.com>
2016-01-10 19:11:52 +0100

    Inlamningsuppgift2 Complete!

---
+++ Inlamningsuppgift2/default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Inlamningsuppgift2._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+        <asp:Label ID="Label1" runat="server" Text="Label" Visible="true"></asp:Label>
+        <asp:Button ID="Button1" runat="server" Text="Add to database" OnClick="Button1_Click" />
+        <asp:TextBox ID="AddToDatabase" runat="server" Text="Write the name of your product here"></asp:TextBox>
+        <asp:Label ID="ErrorLabel" runat="server" Text="" Visible="true"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Inlamningsuppgift2/default.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Inlamningsuppgift2
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SqlConnection sqlConnection = new SqlConnection("Data Source=localhost;Initial Catalog=AdventureWorks2012;User ID=RamiGranat1; Password=hajtech1; Integrated security=true;");
+
+            string sqlQuery = "SELECT * FROM Production.ProductCategory";
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, sqlConnection);
+
+            SqlDataReader oreader;
+
+            try
+            {
+                ListItem newItem = new ListItem();
+                newItem.Text = "Select";
+                newItem.Value = "0";
+                DropDownList1.Items.Add(newItem);
+                sqlConnection.Open();
+                oreader = cmd.ExecuteReader();
+                while (oreader.Read())
+                {
+                    ListItem listItem1 = new ListItem();
+                    listItem1.Text = oreader["Name"].ToString();
+                    listItem1.Value = oreader["ProductCategoryID"].ToString();
+                    DropDownList1.Items.Add(listItem1);
+                }
+                oreader.Close();
+                oreader.Dispose();
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = ex.Message;
+                Label1.Visible = true;
+            }
+            /*
+            datawriter
+
+            
+
+            SqlInsertInto(columns);
+            values('value1', value2)
+
+            */
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+if (!String.IsNullOrWhiteSpace(AddToDatabase.Text))
+            {
+            
+            SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=AdventureWorks2012;User ID=RamiGranat1; Password=hajtech1; Integrated security=true;");
+
+                DateTime newModifiedDate = DateTime.Now;
+                String myGuid = Guid.NewGuid().ToString();
+
+                con.Open();
+
+            String sqlQuery = "INSERT INTO Production.ProductCategory (NAME, rowguid, ModifiedDate) VALUES ('" + AddToDatabase.Text + "','" + myGuid + "','" + newModifiedDate + "')";
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+            if(cmd.ExecuteNonQuery() < 1)
+            {
+                    ErrorLabel.Text = "Kategorien kunde tyvärr inte göras pga av databas fel";
+
+            ErrorLabel.Visible = true;
+            }
+            con.Close();
+            con.Dispose();
+            cmd.Dispose();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
